5cbffb222cbc97ca59d15a001014c5f48126b057
Taylor Dawkins <5dawkt71@solent.ac.uk>
2021-09-30 09:53:00 +0100

    Added code to display escape characters.

---
+++ Basic/Output/multiline_message.py
@@ -0,0 +1,14 @@
+# Display message to the standard output
+print("System failure imminent")
+print("System reboot has been initiated")
+
+print("""
+==============================
+
+
+..... rebooting sensory system
+
+.....rebooting output motor
+
+.....rebooting hover engine
+""")
