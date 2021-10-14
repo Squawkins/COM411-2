4ba1a47b8be96779cea7b9da7f36ba53df754ac8
Squawkins <5dawkt71@solent.ac.uk>
2021-10-14 13:34:18 +0100

    Made my own Review goign over all input code covered

---
+++ Basic/Input/Review.py
@@ -0,0 +1,9 @@
+print("What is your name?")
+name == input()
+
+
+print(f"\n\nHello {name} I am beep, how many hearts should I have?")
+hearts == input()
+
+heartsicon = "♥"
+print(f"I have \n{heartsicon * int(hearts)} hearts\nThank You :)")
