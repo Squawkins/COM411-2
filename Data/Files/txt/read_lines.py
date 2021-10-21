2c88a8c10a23d7351b3b95e82a360440638f2154
Squawkins <5dawkt71@solent.ac.uk>
2021-10-21 14:55:10 +0100

    Added code to read a text file line by line. Although it is slightly incorrect.

--- Data/Files/txt/read_lines.py
+++ Data/Files/txt/read_lines.py
@@ -1,11 +1,10 @@
 def search(file_name):
     print("Searching...")
     with open(file_name) as file:
-        data = file.read()
-        for line in data:
-            location = line.readfile()
-            print(f"Looked in {location}")
-        print("\n\n...Done!")
+        data = file.readline()
+        location = file.readlines()
+        print(f"Looked in {location}")
+    print("...Done")
 
 
 def run():
