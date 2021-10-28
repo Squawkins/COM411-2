aadac110946ca5fbda92d04f2c591eb99699f8a4
Squawkins <5dawkt71@solent.ac.uk>
2021-10-28 15:20:09 +0100

    Added code to read a JSON file. not finsihed completely

--- Data/Files/txt/Search.py
+++
@@ -1,5 +0,0 @@
-def search(file_path):
-    print("Searching...\n\n")
-    str = ""
-    str1 = "books:\n"
-    with open(Books.txt) as file:
--- Data/Files/txt/read_lines.py
+++ Data/Files/txt/read_lines.py
@@ -1,14 +1,13 @@
-def search(file_name):
+def search(file_path):
     print("Searching...")
-    with open(file_name) as file:
-        data = file.readline()
-        location = file.readlines()
-        print(f"Looked in {location}")
-    print("...Done")
+    with open(file_path) as file:
+        for location in file:
+            print(f"Looked in {location.strip()}")
+    print("Done!")
 
 
 def run():
-    search('library.txt')
+    search("library.txt")
 
 
 if __name__ == "__main__":
